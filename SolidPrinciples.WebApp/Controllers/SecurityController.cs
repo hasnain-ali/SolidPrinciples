0c4d5197c051581e9dd335fa7b9f6b28202538d7
Hasnain Ali <hasnain.ali@achilles.com>
2017-11-13 14:14:02 +0000

    Initial web project files.

---
+++ SolidPrinciples.WebApp/Controllers/SecurityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SolidPrinciples.WebApp.Controllers
+{
+    public class SecurityController : Controller
+    {
+        // GET: Security
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+    }
+}
---
+++ SolidPrinciples.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SolidPrinciples.WebApp.MvcApplication" Language="C#" %>
