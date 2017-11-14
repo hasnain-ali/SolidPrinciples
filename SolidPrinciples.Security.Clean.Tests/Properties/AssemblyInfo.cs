fd4e8c23a9b7188339d861b0725b8bbdb4384cd6
Hasnain Ali <hasnain.ali@achilles.com>
2017-11-14 11:35:32 +0000

    Amended to add a test project for clean library supporting Xunit testing framework.

---
+++ SolidPrinciples.Security.Clean.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SolidPrinciples.Security.Clean.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SolidPrinciples.Security.Clean.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1f86715-ef85-4eab-a17b-2381ad59f016")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
