bed64d5866713537006d27de7c14bf23dcdd47e3
Hasnain Ali <hasnain.ali@achilles.com>
2017-12-19 15:55:29 +0000

    Converted all unit test project to C# library project to remove MS Test framework references to avoid any build issue on Travis.

--- SolidPrinciples.Security.Clean.Tests/Properties/AssemblyInfo.cs
+++ SolidPrinciples.Security.Clean.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("SolidPrinciples.Security.Clean.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,10 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("d1f86715-ef85-4eab-a17b-2381ad59f016")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2828a250-40a3-4c2d-9e8f-77a61376d2a2")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
